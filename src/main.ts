79b10c6387cd818546f2b723f577d968f4cbbcb6
jeremiah33v3 <rggammad@gmail.com>
2026-09-16 11:11:27 -0500

    Update browser favicon

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import favicon from './assets/Gammad_Icon_Transparent.png'
+
+document.querySelector<HTMLLinkElement>('link[rel="icon"]')?.setAttribute('href', favicon)
 
 createApp(App).mount('#app')
