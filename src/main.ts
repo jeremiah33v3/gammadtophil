f412fe07e3a6be9c7ad50c6907b8179e245189c1
jeremiah33v3 <rggammad@gmail.com>
2026-09-25 16:17:16 -0500

    Start every page load at home

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import App from './App.vue'
 import favicon from './assets/Gammad_Icon_Transparent.png'
 
 document.querySelector<HTMLLinkElement>('link[rel="icon"]')?.setAttribute('href', favicon)
+history.scrollRestoration = 'manual'
+if (window.location.hash) history.replaceState(null, '', `${window.location.pathname}${window.location.search}`)
+window.scrollTo(0, 0)
 
 createApp(App).mount('#app')
